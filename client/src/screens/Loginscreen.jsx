bbc1af55ec1ef26e7d0b6c74d744b58cce35df98
Amarjeet Kumar <141044796+Amarjeetkumar7@users.noreply.github.com>
2025-06-20 13:17:14 +0530

    Update Loginscreen.jsx

--- client/src/screens/Loginscreen.jsx
+++ client/src/screens/Loginscreen.jsx
@@ -27,7 +27,7 @@ function Loginscreen() {
     // }
 
     try {
-      const res = await axios.post('http://server-hotel-azure.vercel.app/api/users/login', user);
+      const res = await axios.post('https://server-hotel-xgba.onrender.com/api/users/login', user);
       console.log('response is', res)
 
       if (!res.data?.message) {
